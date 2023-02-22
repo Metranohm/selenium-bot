90cb8d4bf4dd14fd9ac592670a8a574c3142a230
Metranohm <andrewwink@gmail.com>
2023-02-21 18:34:10 -0600

    compose test

---
+++ bot/booking/booking.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+import os 
+import booking.constants as const  
+
+
+class Booking(webdriver.Chrome):
+    def __init__(self, driver_path=r"usr/local/bin/chromedriver"):
+        self.driver_path = driver_path
+        os.environ['PATH'] += self.driver_path
+        super(Booking, self).__init__()
+        
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.quit()
+        
+    def land_first_page(self):
+        self.get(const.BASE_URL)
+    
---
+++ bot/main.py
@@ -0,0 +1,8 @@
+from booking.booking import Booking
+import time
+
+
+with Booking() as bot:
+  bot.land_first_page()
+  print("Exiting...")
+
