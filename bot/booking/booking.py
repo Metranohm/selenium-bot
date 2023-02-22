e26bbf8ee49c54f8eefae02448fe5d2d2607853f
Metranohm <andrewwink@gmail.com>
2023-02-21 18:36:10 -0600

    add teardown

--- bot/booking/booking.py
+++ bot/booking/booking.py
@@ -4,12 +4,14 @@ import booking.constants as const
 
 
 class Booking(webdriver.Chrome):
-    def __init__(self, driver_path=r"usr/local/bin/chromedriver"):
+    def __init__(self, driver_path=r"usr/local/bin/chromedriver", teardown=False):
         self.driver_path = driver_path
+        self.teardown = teardown
         os.environ['PATH'] += self.driver_path
         super(Booking, self).__init__()
         
     def __exit__(self, exc_type, exc_val, exc_tb):
+      if self.teardown:
         self.quit()
         
     def land_first_page(self):
